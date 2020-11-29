3605bed4365e659f8ca8e830d85ecc5b66319077
Mansour Jalaly <mansour.jalaly@kcl.ac.uk>
2020-11-29 21:02:18 +0000

    EDIT: Blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -16,11 +16,15 @@ class Blockchain(object):
     difficulty_target = "0000"
 
 # Creates a class named blockchain with two methods
-# hash_block() method encodes a block into array of bytes and then hashes it; you need to ensure that the dictionary is sorted, or you'll have inconsistent hashes later on
-# __init__() function is the constructor for the class. Here, you store the entire blockchain as a list. 
-# Because every blockchain as genesis block, you need to initialise the genesis block with the hash of the previous block, and in this example, 
+# hash_block() method encodes a block into array of bytes and then hashes it; you need to
+# ensure that the dictionary is sorted, or you'll have inconsistent hashes later on
+# __init__() function is the constructor for the class. Here, you store the entire
+# blockchain as a list. 
+# Because every blockchain as genesis block, you need to initialise the genesis block with
+# the hash of the previous block, and in this example, 
 # we simply used a fixed string called "genesis_block" to obtain the hash.
-# Once the hash of the previous block is found, we need to find the nonce for the block using the method named proof_of_work()
+# Once the hash of the previous block is found, we need to find the nonce for the block
+# using the method named proof_of_work()
 
     def hash_block(self, block):
         block_encoded = json.dumps(block, sort_keys=True).encode()
@@ -31,7 +35,8 @@ class Blockchain(object):
         self.chain = [] 
         # temporarily stores the transactions for the current block
         self.current_transactions = []
-        # create the genesis block with a specific fixed hash of previous block genesis block starts with index 0
+        # create the genesis block with a specific fixed hash of previous block genesis
+        # block starts with index 0
         genesis_hash = self.hash_block("genesis_block")
         self.append_block(
             hash_of_previous_block = genesis_hash,
@@ -50,7 +55,8 @@ class Blockchain(object):
         return nonce
 
     def valid_proof(self, index, hash_of_previous_block, transactions, nonce):
-        # Create a string containing the hash of the previous block and block content, including the nonce
+        # Create a string containing the hash of the previous block and block content,
+        # including the nonce
         content = f"{index}{hash_of_previous_block}{transactions}{nonce}".encode()
         # Hash using sha256
         content_hash = hashlib.sha256(content).hexdigest()
@@ -61,7 +67,8 @@ class Blockchain(object):
     # Creates a new block and adds it to the blockchain
     def append_block(self, nonce, hash_of_previous_block):
         
-        # When the block is added to the blockchain, the current timestamp is also added to the block
+        # When the block is added to the blockchain, the current timestamp is also added
+        # to the block
 
         block = {
             'index' : len(self.chain),
